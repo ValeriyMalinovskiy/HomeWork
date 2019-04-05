a2b249a574102d05f130725c05a63ec318e4e4db
Valer Malin <mdfcnvsn@gmail.com>
2019-04-05 20:42:50 +0300

    Second task added

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -10,6 +10,20 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
+            Task2();
+            Console.ReadKey();
+        }
+        static void Task1()
+        {
+            int x1 = int.MinValue;
+            Console.WriteLine(x1);
+        }
+        static void Task2()
+        {
+            Console.WriteLine("Ваше имя?");
+            string str1 = Console.ReadLine();
+            string str2 = "Привет, " + str1;
+            Console.WriteLine(str2);
         }
     }
 }
