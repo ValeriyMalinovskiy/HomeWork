2120e7d96936d4978b116ce054f5948fc0c3b8c3
Valer Malin <mdfcnvsn@gmail.com>
2019-04-22 21:23:56 +0300

    Task1 added.

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -10,6 +10,25 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
+            Task1();
+
+            Console.ReadKey();
+        }
+
+        static void Task1()
+        {
+            string str = "How can mirrors be real if our eyes aren't real";
+            StringBuilder sb = new StringBuilder();
+            for (int i= 0; i<str.Length;i++)
+            {
+                if ((i==0)||(str[i-1]==' '))
+                {
+                    sb.Append(str[i].ToString().ToUpper());
+                }
+                else
+                sb.Append(str[i]);
+            }
+            Console.WriteLine(sb);
         }
     }
 }
