c1dd23698a1f8385b45806ffed1cc14761d9b476
Valer Malin <mdfcnvsn@gmail.com>
2019-05-17 23:21:46 +0300

    Gallery task added

---
+++ Gallery/Gallery.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gallery
+{
+    class Gallery
+    {
+        public Plant[] CreateGallery()
+        {
+            Plant[] plants =
+            {
+                new Tree(3, 20),
+                new Flower(1, 40, ConsoleColor.Blue),
+            };
+            return plants;
+        }
+
+        public void HireWorker(Plant[] plants)
+        {
+            Worker vasya = new Worker();
+            Tuple<int, int> vasaysDecision;
+            for (int i = 0; i < 50; i++)
+            {
+                foreach (var item in plants)
+                {
+                    vasaysDecision = vasya.TakeSomeAction();
+                    item.ChangeState((vasaysDecision.Item1), (vasaysDecision.Item2));
+                }
+            }
+        }
+    }
+}
