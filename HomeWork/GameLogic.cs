cbb886f79c6ba8ea3eb9fde4bfc753b8bab3272b
Valer Malin <mdfcnvsn@gmail.com>
2019-08-26 12:40:08 +0300

    respawn timing adjusted

--- HomeWork/GameLogic.cs
+++ HomeWork/GameLogic.cs
@@ -93,7 +93,7 @@ namespace HomeWork
                                 break;
                             case false:
                                 {
-                                    Thread.Sleep((int)(rnd.Next(1000, 1500) / this.level));
+                                    Thread.Sleep((int)(rnd.Next(1200, 1700) / this.level));
                                 }
                                 break;
                         }
